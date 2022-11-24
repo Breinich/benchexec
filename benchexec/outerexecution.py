c0d755d33af8e069ed18b7005c22b8fd64d90477
Breinich <vencel@bajnok.hu>
2022-11-24 09:26:31 +0100

    outerexecution clean-up phase 1

--- benchexec/outerexecution.py
+++ benchexec/outerexecution.py
@@ -339,11 +339,9 @@ class _Worker(threading.Thread):
         # pqos = Pqos()
         # if self.my_cpus:
         #     pqos.start_monitoring([self.my_cpus])
-
-        # argumentumok kiírsa file-ba
-        # + bővítési lehetőségek:
-        # rögtön az ütemező meghívása
-
+        #
+        #   Parameters of the execution method:
+        #
         #     args,
         #     output_filename=run.log_file,
         #     output_dir=run.result_files_folder,
@@ -358,9 +356,10 @@ class _Worker(threading.Thread):
         #     workingDir=benchmark.working_directory(),
         #     maxLogfileSize=benchmark.config.maxLogfileSize,
         #     files_count_limit=benchmark.config.filesCountLimit,
-        #     files_size_limit=benchmark.config.filesSizeLimit,
+        #     files_size_limit=benchmark.config.filesSizeLimit
+        #
 
-        # print the commands of the scenarios to run in csv for runexec
+        # print the commands of the scenarios to run to a csv file for runexec
         with open('command.csv', mode='a') as output:
             output_writer = csv.writer(output, delimiter=';', quotechar='"', quoting=csv.QUOTE_NONNUMERIC)
             output_writer.writerow([args,
@@ -381,175 +380,40 @@ class _Worker(threading.Thread):
                                     benchmark.config.containerargs],
                                    )
 
+        # TODO szétválasztani a beolvasást és a kiírást
 
-        with open('command.csv') as input:
-            input_reader = csv.reader(input, delimiter=';')
-            for row in input_reader:
-                args2 = row[0][1:-1].split(', ')
-                i = 0
-                for a in args2:
-                    a = a[1:-1]
-                    args2[i] = a
-                    i = i + 1
-                out_fn = row[1]
-                out_dir = row[2]
-                res_fp = row[3]
-                hardtl = None
-                if row[4].isnumeric():
-                    hardtl = int(row[4])
-                softtl = None
-                if row[5].isdigit():
-                    softtl = int(row[5])
-                walltl = None
-                if row[6].isnumeric():
-                    walltl = int(row[6])
-                cores = None
-                if row[7] != '':
-                    tmp0 = row[7][1:-1]
-                    tmp1 = tmp0.split(',')
-                    cores = [eval(i) for i in tmp1]
+        # Reading the result of one executed run scenario
+        configs = Properties()
+        with open('result.properties', 'rb') as config_file:
+            configs.load(config_file)
 
-                mem_ns = None
-                if row[8] != "":
-                    tmp0 = row[8][1:-1]
-                    tmp1 = tmp0.split(',')
-                    mem_ns = [eval(i) for i in tmp1]
+        items_view = configs.items()
+        result_dict = {}
 
-                mem_l = None
-                if row[9].isnumeric():
-                    mem_l = int(row[9])
+        for item in items_view:
+            if item[0] == 'exitcode':
+                result_dict[item[0]] = util.ProcessExitCode(
+                    raw=cast(Optional[float], item[1].data[16:-1].split(', ')[0].split('=')[1]),
+                    value=cast(Optional[float], item[1].data[16:-1].split(', ')[1].split('=')[1]),
+                    signal=cast(Optional[float], item[1].data[16:-1].split(', ')[2].split('=')[1]))
 
-                env = {}
-                if row[10] != "{}":
-                    env = dict((a[1:-1], b[1:-1])
-                               for a, b in (element.split(':')
-                                            for element in row[10][1:-1].split(', ')))
-                w_dir = row[11]
+            elif item[0] == 'starttime':
+                result_dict[item[0]] = item[1].data
 
-                max_lfs = None
-                if row[12].isnumeric():
-                    max_lfs = int(row[12])
+            else:
+                if re.compile('.*\..*').match(item[1].data):
+                    result_dict[item[0]] = float(item[1].data)
 
-                files_cl = None
-                if row[13].isnumeric():
-                    files_cl = int(row[13])
+                elif re.compile('[0-9]').match(item[1].data):
+                    result_dict[item[0]] = int(item[1].data)
 
-                files_sl = None
-                if row[14].isnumeric():
-                    files_sl = int(row[14])
+                else:
+                    result_dict[item[0]] = item[1].data
 
-                param_dict = {}
-                if row[15] != "{}":
-                    temp1 = row[15][1:-1].split('{')
-                    temp2 = temp1[1].split('}')
-                    temp3 = temp1[0].split(', ')
+        run.set_result(result_dict)
 
-                    # key of the value, that is a dict
-                    xtra = temp3.pop(-1)[1:-3]
-
-                    for element in temp3:
-                        element = element.split(': ')
-                        param_dict[element[0][1:-1]] = element[1]
-
-                    val_dict = {}
-                    if len(temp2) > 1:
-                        for stuff in temp2[0].split(', '):
-                            stuff = stuff.split(': ')
-                            val_dict[stuff[0][1:-1]] = stuff[1][1:-1]
-                    param_dict[xtra] = val_dict
-
-                for item in temp2[1].split(', '):
-                    if item != "":
-                        item = item.split(': ')
-                        param_dict[item[0][1:-1]] = item[1]
-
-                for key in param_dict.keys():
-                    if param_dict.get(key) == "True":
-                        param_dict[key] = True
-                    elif param_dict.get(key) == "False":
-                        param_dict[key] = False
-
-                run_result = RunExecutor(**param_dict).execute_run(
-                    args2,
-                    output_filename=out_fn,
-                    output_dir=out_dir,
-                    result_files_patterns=res_fp,
-                    hardtimelimit=hardtl,
-                    softtimelimit=softtl,
-                    walltimelimit=walltl,
-                    cores=cores,
-                    memory_nodes=mem_ns,
-                    memlimit=mem_l,
-                    environments=env,
-                    workingDir=w_dir,
-                    maxLogfileSize=max_lfs,
-                    files_count_limit=files_cl,
-                    files_size_limit=files_sl,
-                )
-
-                # with open("result.txt", "w") as res_txt:
-                 #    res_txt.write(run_result.__str__())
-                #     res_txt.close()
-
-                result_file = open("result.properties", "w")
-
-                result = run_result.copy()
-                exit_code = cast(Optional[util.ProcessExitCode], result.pop("exitcode", None))
-
-                def print_optional_result(file, key, b=False):
-                    if key in result:
-                        file.write(f"{key}={result[key]}\n")
-                        if b:
-                            print(key, result[key])
-
-
-
-                # output results
-                print_optional_result(result_file, "starttime")
-                print_optional_result(result_file, "terminationreason")
-
-                # if exit_code is not None and exit_code.value is not None:
-                #     result_file.write(f"returnvalue={exit_code.value}\n")
-                # if exit_code is not None and exit_code.signal is not None:
-                #     result_file.write(f"exitsignal={exit_code.signal}\n")
-
-                result_file.write(f"exitcode={repr(exit_code)}\n")
-
-                print_optional_result(result_file, "walltime")
-                print_optional_result(result_file, "cputime")
-                for key in sorted(result.keys()):
-                    if key.startswith("cputime-"):
-                        result_file.write(f"{key}={result[key]:.9f}\n")
-                print_optional_result(result_file, "memory")
-                print_optional_result(result_file, "blkio-read")
-                print_optional_result(result_file, "blkio-write")
-                print_optional_result(result_file, "exitcode", True)
-
-                result_file.close()
-
-                configs = Properties()
-                with open('result.properties', 'rb') as config_file:
-                    configs.load(config_file)
-
-                items_view = configs.items()
-                result_dict = {}
-
-                for item in items_view:
-                    if item[0] == 'exitcode':
-
-                        # print(item[1].data)
-                        result_dict[item[0]] = util.ProcessExitCode(raw=cast(Optional[float], item[1].data[16:-1].split(', ')[0].split('=')[1]), value=cast(Optional[float], item[1].data[16:-1].split(', ')[1].split('=')[1]), signal=cast(Optional[float], item[1].data[16:-1].split(', ')[2].split('=')[1]))
-                    elif item[0] == 'starttime':
-                        result_dict[item[0]] = item[1].data
-                    else:
-                        if re.compile('.*\..*').match(item[1].data):
-                            result_dict[item[0]] = float(item[1].data)
-                        elif re.compile('[0-9]').match(item[1].data):
-                            result_dict[item[0]] = int(item[1].data)
-                        else:
-                            result_dict[item[0]] = item[1].data
-
-                print(result_dict)
+        self.output_handler.output_after_run(run)
+        return None
 
         #     args,
         #     output_filename=run.log_file,
@@ -596,11 +460,6 @@ class _Worker(threading.Thread):
         # if self.my_memory_nodes:
         #     run_result["memoryNodes"] = self.my_memory_nodes
 
-        # result processing
-        run.set_result(result_dict)
-
-        self.output_handler.output_after_run(run)
-        return None
 
     def stop(self):
         # asynchronous call to runexecutor,
